a0dd4f9d4d83cbd7e71867182bb76c98b1c8749d
Alexandra DeWit <alexa.dewit@gmail.com>
2017-12-28 01:47:13 +0100

    The elmventure beginst

---
+++ src/App.elm
@@ -0,0 +1,45 @@
+module App exposing (..)
+
+import Html exposing (Attribute, Html)
+
+
+type alias Model =
+    {}
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( {}
+    , Cmd.none
+    )
+
+
+type Msg
+    = NoOp
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        NoOp ->
+            ( model, Cmd.none )
+
+
+view : Model -> Html Msg
+view model =
+    Html.text "Hello World"
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+main : Program Never Model Msg
+main =
+    Html.program
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = subscriptions
+        }
